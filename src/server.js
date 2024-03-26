f2f88b6644626655cc422aa1b5fb449134dc9fdd
Guilberth12 <Guilbertw@gmail.com>
2024-03-26 12:57:26 -0400

    Versão para publicação no Heroku

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const perfilRoute = require("./routes/PerfilRoute")
 const notificacaoRoute = require("./routes/NotificacaoRoute");
 const loginRoute = require("./routes/LoginRoute")
 
+const cors = require("cors")
 const api = express()
 const db = require("./db")
 
@@ -10,6 +11,12 @@ db.connect()
 
 
 api.use(express.json())
+api.use(
+    cors({
+        origin: "*"
+    })
+)
+
 api.use("/perfil", perfilRoute);
 api.use('/notificacao', notificacaoRoute);
 api.use("/login", loginRoute)
